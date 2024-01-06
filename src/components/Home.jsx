77fbdb87a8ee849d3610d168e80095c4a9b97d46
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-06 10:24:10 +0100

    Adjusting home page sizes and continuing with the menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next'
 import Drink from '../assets/Bebida.png'
 import Background from '../assets/Vector.png'
 import Plate from '../assets/Plate.png'
+import { Link } from 'react-router-dom'
+
 
 
 export default function Home() {
@@ -11,21 +13,22 @@ export default function Home() {
   const {t} = useTranslation();
 
   return (
-    <div>
-    <main className='container'>
+    <main className=''>
       <div className='py-2 px-12'>
         <h2 className='text-5xl w-2/4 text-[#E6328B] font-bold my-10'>{t('title')}</h2>
         <p className='text-lg text-gray-600'>{t('subtitle')}</p>
         <p className='text-lg text-gray-600'>{t('subtitle2')}</p>
-        <button className='w-32 h-9 rounded-lg my-8 bg-[#E6328B] bg-opacity-45 text-white font-bold text-lg shadow-gray-300 shadow-md'>
-          {t('ourmenu')}
-        </button>
+        <Link to={"/menu"}>
+          <button className='w-32 h-9 rounded-lg my-8 bg-[#E6328B] bg-opacity-45 text-white font-bold text-lg shadow-gray-300 shadow-md'>
+            {t('ourmenu')}
+          </button>
+        </Link>
         <p className='charm text-2xl'>{t('slogan')} <span className='text-3xl bg-gradient-to-r from-[#E6328B] to-[#E2DD9A] text-transparent bg-clip-text'>{t('gozice')}</span></p>
       </div>
       <div className=''>
-        <img className='absolute top-14 right-0 w-[380px]' src={Drink} alt="" />
-        <img className='absolute top-0 right-0 w-[700px] h-[100vh] -z-20' src={Background} alt="" />
-        <img className='absolute top-32 right-80 w-[250px]' src={Plate} alt="" />
+        <img className='absolute top-14 right-0 w-[350px]' src={Drink} alt="" />
+        <img className='absolute top-0 right-0 w-[700px] h-screen -z-20' src={Background} alt="" />
+        <img className='absolute top-28 right-80 w-[250px]' src={Plate} alt="" />
       </div>
       <div className='flex items-center gap-2 text-2xl absolute bottom-2 left-2'>
         <Icon icon="mdi:instagram" />
@@ -33,7 +36,7 @@ export default function Home() {
         <Icon icon="ic:baseline-facebook" />
       </div>
     </main>
-    </div>
+ 
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,43 +4,77 @@ import ClassicBowl from '../assets/ClassicAcai.png'
 import BlueChiaBowl from '../assets/BlueChia.png'
 import RainbowBowl from '../assets/Rainbow.png'
 import BlissPeanutBowl from '../assets/BlissPeanut.png'
+import Mangozice from '../assets/Mangozice.png'
+import Chocozice from '../assets/Chocozice.png'
+import Avozice from '../assets/Avozice.png'
+import Detozice from '../assets/Detozice.png'
+import Oranzice from '../assets/Oranzice.png'
+import ClassicSmoothie from '../assets/ClassicSmoothie.png'
+import Butterzice from '../assets/Butterzice.png'
 
 export default function Menu() {
 
   const {t} = useTranslation();
 
   return (
-    <div>
+    <div className='scroll'>
       <h2 className='m-8 text-[#E6328B] font-bold text-2xl'>{t('bowls')}</h2>
         <div className='grid grid-cols-3 items-center gap-8 my-10 mx-6'>
           <div className='flex justify-center items-center'>
             <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={ClassicBowl} alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center'>Classic Acai Bowl</h3>
-              <p className='text-[#A5A3A3] w-72 text-center text-sm'>Acai, granola, peanut butter, banana, strawberry, blueberry & coconut</p>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>Acai, granola, peanut butter, banana, strawberry, blueberry & coconut</p>
             </div>
           </div>
           <div className='flex justify-center items-center'>
             <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={RainbowBowl} alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center'>Rainbow Acai Bowl</h3>
-              <p className='text-[#A5A3A3] w-72 text-center text-sm'>Acai, mango, pitaya, granola, banana, strawberry & blueberry</p>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>Acai, mango, pitaya, granola, banana, strawberry & blueberry</p>
             </div>
           </div>
           <div className='flex justify-center items-center'>
             <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={BlueChiaBowl} alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center'>Blue Chia Acai Bowl</h3>
-              <p className='text-[#A5A3A3] w-72 text-center text-sm'>Acai, blue chia, granola, almond butter, banana, blueberry & hemp seeds</p>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>Acai, blue chia, granola, almond butter, banana, blueberry & hemp seeds</p>
             </div>
           </div>
           <div className='flex justify-center items-center'>
             <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={BlissPeanutBowl} alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center'>Bliss Peanut Acai Bowl</h3>
-              <p className='text-[#A5A3A3] w-72 text-center text-sm'>Acai, granola, banana, strawberry, cacao & double peanut butter.</p>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>Acai, granola, banana, strawberry, cacao & double peanut butter.</p>
             </div>
           </div>
+        </div>
+        <h2 className='m-8 text-[#E6328B] font-bold text-2xl'>{t('smoothies')}</h2>
+        <div className='grid grid-cols-3 items-center gap-8 my-10 mx-6'>
+        <div className='flex justify-center items-center'>
+            <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={ClassicSmoothie} alt="" />
+            <div className=''>
+              <h3 className='text-lg uppercase text-center'>Classic Smoothie</h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>strawberrys, banana, non fat yogurt & white chocolate</p>
+            </div>
+          </div>
+          {/* CHOCOZICE */}
+          <div className='flex justify-center items-center'>
+            <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={Chocozice} alt="" />
+            <div className=''>
+              <h3 className='text-lg uppercase text-center'>Chocozice Smoothie</h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>chocolate, cocounat, non fat yogurt & banana</p>
+            </div>
+          </div>
+          {/* AVOZICE */}
+          <div className='flex justify-center items-center'>
+            <img className='w-[100px] object-contain transition-transform transform scale-100 hover:scale-125' src={Avozice} alt="" />
+            <div className=''>
+              <h3 className='text-lg uppercase text-center'>Avozice Smoothie</h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-sm'>spinach, green apple, kiwi, ginger & lemon.</p>
+            </div>
+          </div>
+          
         </div>
     </div>
   )
