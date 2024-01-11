f2acb5e8a74b9eec78b0267b15711312ff889671
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-11 15:49:26 +0100

    update

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -8,22 +8,22 @@ export default function Carusel() {
 
   return (
     <div className="overflow-hidden  bg-[#ef226a] border border-black ">
-    <div className="animate-slide flex justify-center items-center gap-3 quick font-bold text-black text-xl">
+    <div className="animate-slide flex justify-center items-center gap-3 quick font-medium text-black text-xl">
       <span className="mx-4">{t('goziceSlider')}</span>
       <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
-      <span className='mx-4 underline'>Còrsega 263</span>
+      <span className='mx-4'>Còrsega 263</span>
       <span className="mx-4">{t('goziceSlider')}</span>
       <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
-      <span className='mx-4 underline'>Còrsega 263</span>
+      <span className='mx-4'>Còrsega 263</span>
       <span className="mx-4">{t('goziceSlider')}</span>
       <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
-      <span className='mx-4 underline'>Còrsega 263</span>
+      <span className='mx-4'>Còrsega 263</span>
       <span className="mx-4">{t('goziceSlider')}</span>
       <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
-      <span className='mx-4 underline'>Còrsega 263</span>
+      <span className='mx-4'>Còrsega 263</span>
       <span className="mx-4">{t('goziceSlider')}</span>
       <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
-      <span className='mx-4 underline'>Còrsega 263</span>
+      <span className='mx-4'>Còrsega 263</span>
     </div>
   </div>
 
