28b3be131a213bc104469121d512db782a8ca9a9
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-11 16:36:13 +0100

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,8 +20,8 @@ export default function Home() {
       <Header/>
       <div className='text-center lg:text-start py-2'>
         <h2 className='my-6 text-3xl px-12 lg:text-5xl lg:w-2/5 text-[#E6328B] font-bold lg:my-6 lg:tracking-wide'>{t('title')}</h2>
-        <p className='px-8 font-semibold lg:text-lg text-gray-600'>{t('subtitle')}</p>
-        <p className='px-8 font-semibold lg:text-lg text-gray-600'>{t('subtitle2')}</p>
+        <p className='bai px-8 font-semibold lg:text-lg text-gray-600'>{t('subtitle')}</p>
+        <p className='bai px-8 font-semibold lg:text-lg text-gray-600'>{t('subtitle2')}</p>
         <Link to={"/menu"}>
           <button className='bai w-32 h-9 rounded-full mt-6 mb-6 lg:my-8 bg-[#E6328B] bg-opacity-45 text-white font-bold text-lg shadow-[#E6328B] shadow-md'>
             {t('ourmenu')}
@@ -30,7 +30,7 @@ export default function Home() {
         <p className='charm text-xl lg:text-2xl'>{t('slogan')} <span className='text-3xl bg-gradient-to-r from-[#dad266] to-[#E6328B] text-transparent bg-clip-text'>{t('gozice')}</span></p>
       </div>
       <div className=''>
-        <img className='absolute -bottom-2 right-0 w-[145px] lg:right-0 lg:top-56 lg:w-[250px]' src={Bebida} alt="" />
+        <img className='absolute bottom-3 right-0 w-[145px] lg:right-0 lg:top-56 lg:w-[250px]' src={Bebida} alt="" />
         <img className='absolute -bottom-2 right-44 w-[150px] lg:top-44 lg:right-80 lg:w-[250px]' src={Plate} alt="" />
       </div>
       <div className='hidden text-xl lg:flex items-center gap-2 lg:text-2xl absolute -bottom-10 left-2'>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,6 +14,7 @@ import ClassicSmoothie from '../assets/ClassicSmoothie.png'
 import Butterzice from '../assets/Butterzice.png'
 import Footer from './Footer'
 import Header from './Header'
+import Carusel from './Carusel'
 
 export default function Menu() {
 
@@ -21,6 +22,7 @@ export default function Menu() {
 
   return (
     <div>
+      <Carusel/>
       <Header/>
     <div className='grid grid-cols-1 justify-center items-center scroll'>
       <h2 className='text-center lg:text-start mt-8 lg:m-8 text-[#E6328B] font-bold text-2xl lg:my-12 uppercase'>{t('bowls')}</h2>
@@ -28,29 +30,29 @@ export default function Menu() {
           <div className='flex-col lg:flex-row flex justify-center items-center'>
             <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={ClassicBowl} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Classic<span className='flex items-center justify-center'>Acai Bowl</span></h3>
-              <p className='text-[#B2B2B2] w-72 text-center text-sm'>{t('classic')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Classic<span className='flex items-center justify-center'>Acai Bowl</span></h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('classic')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
             <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={RainbowBowl} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Rainbow<span className='flex items-center justify-center'>Acai Bowl</span></h3>
-              <p className='text-[#B2B2B2] w-72 text-center text-sm'>{t('rainbow')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Rainbow<span className='flex items-center justify-center'>Acai Bowl</span></h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('rainbow')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
             <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={BlueChiaBowl} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Blue Chia<span className='flex items-center justify-center'>Acai Bowl</span></h3>
-              <p className='text-[#B2B2B2] w-72 text-center text-sm'>{t('blue')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Blue Chia<span className='flex items-center justify-center'>Acai Bowl</span></h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('blue')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
             <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={BlissPeanutBowl} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Bliss Peanut<span className='flex items-center justify-center'>Acai Bowl</span></h3>
-              <p className='text-[#B2B2B2] w-72 text-center text-sm'>{t('bliss')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Bliss Peanut<span className='flex items-center justify-center'>Acai Bowl</span></h3>
+              <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('bliss')}</p>
             </div>
           </div>
         </div>
@@ -59,56 +61,56 @@ export default function Menu() {
         <div className='flex-col lg:flex-row flex justify-center items-center gap-3 mt-2 mb-6 mx-14'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={ClassicSmoothie} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Classic<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('classicSmoothie')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Classic<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('classicSmoothie')}</p>
             </div>
           </div>
           {/* CHOCOZICE */}
           <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Chocozice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Chocozice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('chocozice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Chocozice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('chocozice')}</p>
             </div>
           </div>
           {/* AVOZICE */}
           <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Avozice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Avozice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('avozice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Avozice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('avozice')}</p>
             </div>
           </div>
           {/* DETOZICE */}
           <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Detozice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Detozice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('detozice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Detozice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('detozice')}</p>
             </div>
           </div>
            {/* MANGOZICE */}
            <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Mangozice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Mangozice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('mangozice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Mangozice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('mangozice')}</p>
             </div>
           </div>
           {/* ORANZICE */}
           <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Oranzice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Oranzice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('oranzice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Oranzice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('oranzice')}</p>
             </div>
           </div>
           {/* BUTTERZICE */}
           <div className='flex-col lg:flex-row flex justify-center items-center gap-3'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={Butterzice} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold'>Butterzice<span className='flex items-center justify-center'>Smoothie</span></h3>
-              <p className='text-[#B2B2B2] w-52 text-center text-sm'>{t('butterzice')}</p>
+              <h3 className='text-lg uppercase text-center text-[#6D6E70] font-bold my-2'>Butterzice<span className='flex items-center justify-center'>Smoothie</span></h3>
+              <p className='text-[#B2B2B2] w-52 text-center text-md px-4'>{t('butterzice')}</p>
             </div>
           </div>
         </div>
