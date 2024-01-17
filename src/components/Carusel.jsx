fcdd4bf2b7f690d36586c022efa5b8cc06962273
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-17 12:56:51 +0100

    Enhancing menu with detailed product listings and descriptions

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -10,23 +10,23 @@ export default function Carusel() {
     <div className="overflow-hidden bg-[#ef226a] bg-opacity-90 border border-black">
     <div className="animate-slide flex justify-center items-center gap-3 quick font-medium text-black text-lg">
       <span className="mx-4">{t('goziceSlider')}</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className='mx-4'>Còrsega 263</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className="mx-4">{t('goziceSlider')}</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className='mx-4'>Còrsega 263</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className="mx-4">{t('goziceSlider')}</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className='mx-4'>Còrsega 263</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className="mx-4">{t('goziceSlider')}</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className='mx-4'>Còrsega 263</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className="mx-4">{t('goziceSlider')}</span>
-      <img className='mx-4 w-[60px] h-[60px] m-auto' src={Smile} alt="" />
+      <img className='mx-4 w-[50px] h-[50px] m-auto' src={Smile} alt="" />
       <span className='mx-4'>Còrsega 263</span>
     </div>
   </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import classicBowl from '../assets/classicBowl.png'
+import blueChiaBowl from '../assets/blueChiaBowl.png'
+import rainbowBowl from '../assets/rainbowBowl.png'
+import peanutBowl from '../assets/peanutBowl.png'
 import Mangozice from '../assets/Mangozice.png'
 import Chocozice from '../assets/Chocozice.png'
 import Avozice from '../assets/Avozice.png'
@@ -7,6 +11,9 @@ import Detozice from '../assets/Detozice.png'
 import Oranzice from '../assets/Oranzice.png'
 import ClassicSmoothie from '../assets/ClassicSmoothie.png'
 import Butterzice from '../assets/Butterzice.png'
+import IconSmoothie from '../assets/smoothieIcon.png'
+import AcaiBowlIcon from '../assets/AcaiBowlIcon.png'
+import AcaiIcon from '../assets/AcaiIcon.png'
 import Footer from './Footer'
 import Header from './Header'
 import Carusel from './Carusel'
@@ -20,43 +27,69 @@ export default function Menu() {
       <Carusel/>
       <Header/>
       <div className='my-4 text-center'>
-        <h2 className='text-2xl text-[#2a5d6d] font-serif'>Lorem ipsum</h2>
-        <p className='text-[#2a5d6d] px-4 my-4 font-sans font-medium'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit blanditiis consequuntur ullam perspiciatis, est numquam. Iste, quo minus corporis, quibusdam qui impedit placeat ipsam reprehenderit omnis enim id nemo rerum?</p>
+        <h2 className='mt-8 mb-4 text-3xl text-[#2a5d6d] font-bold lg:my-12'>Nuestro Menú</h2>
+        <p className='text-md text-[#2a5d6d] px-4 lg:px-50 my-4 font-sans font-medium bai'>
+        En Gozice, nos dedicamos a crear momentos de pura delicia y bienestar. Cada uno de nuestros productos ha sido diseñado pensando en ti, combinando calidad superior, ingredientes naturales y un sabor excepcional. Al adentrarte en nuestro menú, descubrirás una selección cuidadosamente elaborada de Açaí Bowls y Smoothies, cada uno con su toque único.
+      </p>
+      <p className='text-md text-[#2a5d6d] px-4 lg:px-50 my-4 font-sans font-medium bai'>
+        <span className='charm font-bold text-xl'> Nuestra misión es simple pero poderosa:</span> brindarte el mejor producto que combine sabor y bienestar.</p>
+      </div>
+      <div className='categories lg:flex justify-center gap-35  items-center my-16'>
+           <div className='my-15 gap-3 flex justify-center items-center flex-col lg:gap-11'> 
+            <img className='w-[60px]' src={AcaiBowlIcon} alt="" />
+              <button style={{ boxShadow: "0px 4px #48889C", border: "border: 1px solid black"}} className='bg-[#009B7E] w-[100px] h-[35px] rounded-full'>
+                <p className='bai text-white text-sm'>Acai Bowl</p>
+              </button>
+           </div>
+       
+            <div className='my-15 gap-3 flex items-center justify-center flex-col lg:gap-8'>
+              <img className='w-[55px]' src={IconSmoothie} alt="" />
+              <button style={{ boxShadow: "0px 4px #48889C", border: "border: 1px solid black"}} className='bg-[#009B7E] w-[100px] h-[35px] rounded-full'>
+                <p className='bai text-white text-sm'>Smoothies</p>
+              </button>
+            </div>
+          
+          <div className='my-8 flex items-center justify-center flex-col lg:gap-4'>
+          <img className='w-[70px] my-3' src={AcaiIcon} alt="" />
+            <button style={{ boxShadow: "0px 4px #48889C", border: "border: 1px solid black"}} className='bg-[#009B7E] w-[100px] h-[35px] rounded-full'>
+              <p className='bai text-white text-sm'>Small Acai</p>
+            </button>
+          </div>
+          
       </div>
     <div className='grid grid-cols-1 justify-center items-center scroll'>
-      <h2 className='text-center lg:text-start my-8 lg:m-8 text-[#E6328B] font-bold text-2xl lg:my-12 uppercase'>{t('bowls')}</h2>
+      <h2 className='text-center my-8 lg:m-8 text-[#E6328B] font-bold text-2xl lg:my-12 uppercase'>{t('bowls')}</h2>
         <div className='grid grid-cols-1 lg:grid lg:grid-cols-3 items-center gap-8 mt-4 mb-12 mx-14'>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
-            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125'  alt="" />
+            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={classicBowl}  alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center text-[#48889C] font-bold my-2'>Classic<span className='flex items-center justify-center'>Acai Bowl</span></h3>
               <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('classic')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
-            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125'  alt="" />
+            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={rainbowBowl}  alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center text-[#48889C] font-bold my-2'>Rainbow<span className='flex items-center justify-center'>Acai Bowl</span></h3>
               <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('rainbow')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
-            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' alt="" />
+            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={blueChiaBowl} alt="" />
             <div className=''>
-              <h3 className='text-lg uppercase text-center text-[#48889C] font-bold my-2'>Rainbow<span className='flex items-center justify-center'>Acai Bowl</span></h3>
+              <h3 className='text-lg uppercase text-center text-[#48889C] font-bold my-2'>BlueChia<span className='flex items-center justify-center'>Acai Bowl</span></h3>
               <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('rainbow')}</p>
             </div>
           </div>
           <div className='flex-col lg:flex-row flex justify-center items-center'>
-            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' alt="" />
+            <img className='w-[100px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={peanutBowl} alt="" />
             <div className=''>
               <h3 className='text-lg uppercase text-center text-[#48889C] font-bold my-2'>Bliss Peanut<span className='flex items-center justify-center'>Acai Bowl</span></h3>
               <p className='text-[#B2B2B2] w-72 text-center text-md px-4'>{t('bliss')}</p>
             </div>
           </div>
         </div>
-        <h2 className='text-center lg:text-start mt-8 lg:m-8 text-[#E6328B] font-bold text-2xl lg:my-12 uppercase'>{t('smoothies')}</h2>
+        <h2 className='text-center mt-8 lg:m-8 text-[#E6328B] font-bold text-2xl lg:my-12 uppercase'>{t('smoothies')}</h2>
         <div className='grid grid-cols-1 lg:grid lg:grid-cols-3 items-center gap-8 my-8 mx-2'>
         <div className='flex-col lg:flex-row flex justify-center items-center gap-3 mt-2 mb-6 mx-14'>
             <img className='w-[70px] lg:object-contain lg:transition-transform lg:transform lg:scale-100 lg:hover:scale-125' src={ClassicSmoothie} alt="" />
